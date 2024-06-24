b575577721909227bfe1fbaacdbcb00831e86f70
Dmitriy <dmitriyworkspace013@gmail.com>
2024-06-24 15:11:05 +0300

    edit scroll

--- src/layouts/MainPage.jsx
+++ src/layouts/MainPage.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header.jsx";
-import Slider from "../components/Slider.jsx";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer.jsx";
 import CheckAuth from "../components/CheckAuth.jsx";
 
-export default function MainPage({ scroll }) {
+export default function MainPage(props) {
 
+  const handleGoToTop = function () {
+    window.scrollTo(0, 0, { behavior: "smooth" })
+  }
 
   return (
     <>
@@ -13,9 +15,9 @@ export default function MainPage({ scroll }) {
         <Header />
     </CheckAuth>
       {
-        scroll > 1200 && (
-              <div className="px-5 py-5 rounded-lg bg-second fixed right-9 bottom-16 mix-blend-difference">
-                <img src={"/ArrowScrollTop.svg"} className="w-[40px]" alt={"Перейти вверх"}/>
+        props.scroll > 1200 && (
+              <div className="px-5 py-5 rounded-lg bg-second fixed right-9 bottom-16 mix-blend-color-dodge cursor-pointer" onClick={handleGoToTop}>
+                <img src={"/ArrowScrollTop.svg"} className="w-[40px] rotate-180" alt={"Перейти вверх"}/>
               </div>
           )
       }
